ca5dc4e4b3fa8f51a45576af5405979fe40c8637
Katrine Birk <katrinebirk2@live.dk>
2022-10-24 11:46:52 +0200

    Choice view 1st draft

--- resources/views/ChooseSite.blade.php
+++ resources/views/ChooseSite.blade.php
@@ -2,10 +2,28 @@
 <html>
 <head>
     <title>Results</title>
-    <link rel="stylesheet" href="{{ asset('styles/StyleTheChoice.css') }}" />
+    <link rel="stylesheet" href="{{ asset('styles/StyleTheChoice.css') }}"/>
 </head>
 <body>
-    <h1>TESST</h1>
+<div>
+    <h1>TEST</h1>
+    <section id="song">
+        <textarea>
+        </textarea>
+    </section>
+    <section id="buttons">
+        <button>
+            Don't like
+        </button>
+        <button>
+            Like
+        </button>
+        <button>
+            Don't know
+        </button>
+
+    </section>
+</div>
 </body>
 
 </html>
