3ee178acc999297826252b8209766d6cdfdf3e39
Kanstrup <krjoh21@student.sdu.dk>
2022-10-24 11:46:22 +0200

    update 0.0001

--- resources/views/TestSite.blade.php
+++ resources/views/TestSite.blade.php
@@ -1,14 +1,27 @@
+<!DOCTYPE html>
 <html>
-
-
-</html>
-
 <head>
-<title></title>
-    style
+    <title>MyPC</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('styles/StyleTheTest.css') }}"/>
 </head>
 
 <body>
+<h1>Pls write 10 songs that you like</h1>
+
+<form>
+    <input type="text" name="LikedSongs"/>
+    <button type="submit">Add song</button>
+</form>
+
+<h1>List of liked songs</h1>
+<ol id="list">
+    
+</ol>
 
 
+<button type="submit">Submit</button>
 </body>
+
+</html>
