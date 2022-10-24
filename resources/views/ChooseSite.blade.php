9346f60cd0db9cba429c69b63a80e047ef3ac9d1
Katrine Birk <katrinebirk2@live.dk>
2022-10-24 10:52:54 +0200

    View 2

---
+++ resources/views/ChooseSite.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Results</title>
+    <link rel="stylesheet" href="{{ asset('styles/StyleTheChoice.css') }}" />
+</head>
+<body>
+    <h1>TESST</h1>
+</body>
+
+</html>
