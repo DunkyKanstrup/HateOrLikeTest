4f790d2bee05f79fd0f1335fdf1a872c300a18de
Kanstrup <krjoh21@student.sdu.dk>
2022-10-31 10:30:32 +0100

    LiveCoding incoming

--- resources/views/TestSite.blade.php
+++ resources/views/TestSite.blade.php
@@ -5,23 +5,34 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('styles/StyleTheTest.css') }}"/>
+
+    <script defer src={{asset("js/TestSite.js")}}></script>
+
 </head>
 
 <body>
-<h1>Pls write 10 songs that you like</h1>
+<h1>Write 10 songs that you like</h1>
 
 <form>
-    <input type="text" name="LikedSongs"/>
-    <button type="submit">Add song</button>
+    <div>
+        <label for="listForm">Input track</label><input list="songList" name="track" id="listForm"/>
+        <datalist id ="songList">
+            <option value ="Caterpillars riding bullets"/>
+            <option value = "Fever Dreams"/>
+            <option value = "Sham Pain"/>
+            <option value = "Check Your Spam"/>
+        </datalist>
+        <button class="add" type="button">Add</button>
+    </div>
 </form>
 
-<h1>List of liked songs</h1>
-<ol id="list">
-    
-</ol>
+<div>
+    <label for = "songPool">Added songs</label>
+    <ol id ="songPool">
 
+    </ol>
+</div>
 
-<button type="submit">Submit</button>
 </body>
 
 </html>
